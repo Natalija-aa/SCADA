35d080d5dc4ad4bbbc62679825cb39bf96d5fbc0
Natalija-aa <natapavlovic03@gmail.com>
2026-06-28 22:00:37 +0200

    Dodati komentari u AnalogInput.cs

--- DataConcentrator/Model/AnalogInput.cs
+++ DataConcentrator/Model/AnalogInput.cs
@@ -19,16 +19,16 @@ namespace DataConcentrator.Model
         public virtual ICollection<Alarm> Alarms { get; set; }
 
         [NotMapped] public double CurrentValue { get; set; }
-        [NotMapped] private Thread scanThread;
+        [NotMapped] private Thread scanThread;  // cita vrednosti bez blokiranja ostatka pprograma
         [NotMapped] private bool isRunning;
 
-        public event Action<string, double> ValueChanged;
+        public event Action<string, double> ValueChanged;   // vrednoist se promenila vise od deadband-a
 
-        public AnalogInput() { Alarms = new List<Alarm>(); }
+        public AnalogInput() { Alarms = new List<Alarm>(); }    // inicijalizuje listu alarma
 
         public void StartScan()
         {
-            if (isRunning) return;
+            if (isRunning) return;  //ako redi vec, ne radi nista
             isRunning = true;
             IsScanning = true;
             scanThread = new Thread(ScanLoop) { IsBackground = true, Name = "Scan_" + Name };
@@ -46,13 +46,15 @@ namespace DataConcentrator.Model
             double prevValue = double.MinValue;
             while (isRunning)
             {
-                Thread.Sleep(ScanTime > 0 ? ScanTime : 1000);
-                double newValue = PLC.Instance.GetValue(IOAddress);
+                Thread.Sleep(ScanTime > 0 ? ScanTime : 1000);    // da ne bi procesor radio na 100% snage 
+                // ako je 0 spava 1s, inace za ScaningTime
+                double newValue = PLC.Instance.GetValue(IOAddress); // vrijednost sa zadate adrese
+                // da li je ovo prvo citanje ili da li je razlika veca od deadbanda
                 if (prevValue == double.MinValue || Math.Abs(newValue - prevValue) >= Deadband)
                 {
                     prevValue = newValue;
                     CurrentValue = newValue;
-                    ValueChanged?.Invoke(Name, newValue);
+                    ValueChanged?.Invoke(Name, newValue);   // javi da se promenila vrednst
                 }
             }
         }
